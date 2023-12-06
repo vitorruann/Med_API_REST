141c9a307820ec54c65eec00f08346deec6d23a7
Vitor Ruan de Souza <vitorruann@gmail.com>
2023-12-06 08:10:04 -0300

    initializing the function to make a schedule

--- src/main/java/med/voll/api/controller/ScheduleController.java
+++ src/main/java/med/voll/api/controller/ScheduleController.java
@@ -1,5 +1,6 @@
 package med.voll.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,17 +9,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
+import med.voll.api.domain.schedule.AppointmentSchedule;
 import med.voll.api.domain.schedule.NewScheduleDTO;
 import med.voll.api.domain.schedule.ScheduleDTO;
 
 @RestController
 @RequestMapping("/schedules")
 public class ScheduleController {
+
+    @Autowired
+    private AppointmentSchedule appointmentSchedule;
     
     @PostMapping
     @Transactional
     public ResponseEntity<ScheduleDTO> storeSchedule(@RequestBody @Valid NewScheduleDTO schedule) {
-        System.out.println(schedule);
+
+        appointmentSchedule.schedule(schedule);
+
         return ResponseEntity.ok(new ScheduleDTO(null, null, null, null));
     }
 }
---
+++ src/main/java/med/voll/api/domain/schedule/AppointmentSchedule.java
@@ -0,0 +1,11 @@
+package med.voll.api.domain.schedule;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AppointmentSchedule {
+    
+    public void schedule(NewScheduleDTO schedule) {
+
+    }
+}
